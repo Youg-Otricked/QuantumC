8dcd2493e17c4c3bf610b0d77308e0b60edb9b87
Youg-Otricked <yougotricked2@gmail.com>
2026-06-02 21:35:10 -0700

    OOPS i forgot to bump up version

--- compiler_run.cpp
+++ compiler_run.cpp
@@ -22,7 +22,7 @@
 #if defined(_WIN32) || defined(_WIN64)
     #include <print>
 #endif
-const std::string ver = "x0.16.6";
+const std::string ver = "x0.17.0";
 #include <random>
 bool slow = false;
 void slow_print(const std::string& text, const std::string& color = "\033[0m", int min_delay_ms = 100, int max_delay_ms = 450) {
