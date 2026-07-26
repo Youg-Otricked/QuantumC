2e85d8e2c9dd20776a503981712627c61a9ac379
Youg-Otricked <yougotricked2@gmail.com>
2026-07-25 17:13:23 -0700

    WASM uses self hosted runtime

--- rebuild-runtime.sh
+++ rebuild-runtime.sh
@@ -1,3 +1,6 @@
 #! /usr/bin/bash
+cmake -S . -B . -DCXX_RUNTIME=ON
+cmake --build . -j$(nproc) 2>build.log --clean-first
+./qc ./runtime.qc -co -nrt -O3 -o runtime.ll
 cmake -S . -B . -DCXX_RUNTIME=OFF
 cmake --build . -j$(nproc) 2>build.log --clean-first
